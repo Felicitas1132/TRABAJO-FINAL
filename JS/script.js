e0b48af669b122f347ddafadd708bbd209c60de1
Melania Cerolini <mcerolini@outlook.com>
2023-06-13 16:27:39 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+ function pregunta(){
+        if (confirm('¿Estas seguro de enviar este formulario? Presiona cancelar si deseas seguir editando')){
+           document.tuformulario.submit()
+        }
+    }
